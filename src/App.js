edfc5b255ddfabceef669dbf0c97f13865f20695
Madeleine Prak <madeleineprak@gmail.com>
2020-03-23 02:27:18 -0700

    Add map widgets

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { Map } from '../src/components/Map/index'
 function App() {
   return (
     <div className="App">
+      <h1>You may have seen the John Hopkins dashboard before</h1>
+      <a href="https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6">John Hopkins</a>
+      <a href="https://who.maps.arcgis.com/apps/opsdashboard/index.html#/c88e37cfc43b4ed3baf977d77e4a0667">WHO</a>
+      <a href="https://storymaps.arcgis.com/stories/4fdc0d03d3a34aa485de1fb0d2650ee0">Story Map</a>
       <Map />
     </div>
   );
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -9,30 +9,70 @@ export class Map extends React.Component {
 
   componentDidMount() {
     // lazy load the required ArcGIS API for JavaScript modules and CSS
-    loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer'], { css: true })
-    .then(([ArcGISMap, MapView, FeatureLayer]) => {
-      const map = new ArcGISMap({
-        basemap: 'topo-vector'
-      });
+    loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer', 'esri/widgets/Locate', 'esri/widgets/Track', 'esri/Graphic', 'esri/widgets/Search'], { css: true })
+      .then(([ArcGISMap, MapView, FeatureLayer, Locate, Track, Graphic, Search]) => {
+        const map = new ArcGISMap({
+          basemap: 'gray'
+        });
 
-      this.view = new MapView({
-        container: this.mapRef.current,
-        map: map,
-        center: [-95.7129, 37.0902],
-        zoom: 3
-      })
-      var cases = new FeatureLayer({
+        this.view = new MapView({
+          container: this.mapRef.current,
+          map: map,
+          center: [-95.7129, 37.0902],
+          zoom: 4
+        })
+
+        var popupCases = {
+          "title": "{Province_State}",
+          content: [{
+            type: "media",
+            mediaInfos: [{
+              type: "column-chart",
+              caption: "",
+              value: {
+                fields: ["Confirmed", "Recovered", "Deaths"],
+                normalizeField: null,
+                tooltipField: "Confirmed cases, recovered cases, and deaths"
+              }
+            }]
+          }]
+        }
+
+        var cases = new FeatureLayer({
           url: "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1",
-          opacity: .5
-      })
+          opacity: .5,
+          color: [0, 112, 255],
+          outfields: ["Province_State", "Confirmed", "Recovered", "Deaths"],
+          popupTemplate: popupCases
+        })
 
-      var deaths = new FeatureLayer({
-        url: "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/0"
-    })
+        var deaths = new FeatureLayer({
+          url: "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/0",
+          opacity: .5,
+          color: [0, 112, 255],
+        })
 
-      map.add(cases)
-      map.add(deaths)
-    });
+        var locate = new Locate({
+          view: this.view,
+          useHeadingEnabled: false,
+          goToOverride: function (view, options) {
+            options.target.scale = 1500;  // Override the default map scale
+            return view.goTo(options.target);
+          }
+        });
+
+        this.view.ui.add(locate, "top-left");
+
+        // Search widget
+        var search = new Search({
+          view: this.view
+        });
+
+        this.view.ui.add(search, "top-right");
+
+        map.add(cases)
+        // map.add(deaths)
+      });
   }
 
   componentWillUnmount() {
