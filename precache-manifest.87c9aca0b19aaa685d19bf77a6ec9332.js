130f3db0eb13f3a6d6dd2177ee68db7fb3461af3
Madeleine Prak <madeleineprak@gmail.com>
2020-03-24 03:35:52 -0700

    Updates

---
+++ precache-manifest.87c9aca0b19aaa685d19bf77a6ec9332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e775eb934a26a92e394813bb4986d236",
+    "url": "/map-project/index.html"
+  },
+  {
+    "revision": "6ce6690379d3de2b2e3a",
+    "url": "/map-project/static/css/main.d0bdce9b.chunk.css"
+  },
+  {
+    "revision": "baf84a067dcf6176b9b1",
+    "url": "/map-project/static/js/2.896a0734.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/map-project/static/js/2.896a0734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce6690379d3de2b2e3a",
+    "url": "/map-project/static/js/main.7dc79f8f.chunk.js"
+  },
+  {
+    "revision": "62fee275bc38c4587679",
+    "url": "/map-project/static/js/runtime-main.7e4b1062.js"
+  }
+]);
